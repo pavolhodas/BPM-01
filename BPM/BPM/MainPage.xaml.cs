62ca856788daa55796d44f9ffe100dabb14bd08f
pavolhodas <pavolhodas4@gmail.com>
2021-01-11 08:43:46 +0100

    Add project files.

---
+++ BPM/BPM/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BPM
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<DateTime> tapTimes;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            tapTimes = new List<DateTime>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            tapTimes.Add(DateTime.Now);
+            if (tapTimes.Count > 2)
+            {
+                var oldest = tapTimes.First();
+                var newest = tapTimes.Last();
+                var duration = newest - oldest;
+                var average = new TimeSpan(duration.Ticks / tapTimes.Count);
+                double bpm = 60 / average.TotalSeconds;
+                bpmLabel.Text = Math.Round(bpm).ToString();
+            }
+        }
+    }
+}
